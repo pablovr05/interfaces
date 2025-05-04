f3f372dedf6333b4315c3c4e29165853507dd1e1
pablo <pablovicenteroura2005@gmail.com>
2025-05-04 14:06:40 +0200

    commit

--- core/src/main/java/com/project/Main.java
+++ core/src/main/java/com/project/Main.java
@@ -3,17 +3,11 @@ package com.project;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
@@ -23,42 +17,42 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void create() {
-        // Crear un Stage i un Skin
+        // Crear el Stage y el Skin
         stage = new Stage(new ScreenViewport());
-        skin = new Skin(Gdx.files.internal("uiskin.json")); // Carregar un Skin per defecte
-
-        // Crear un Label (TextView)
-        Label.LabelStyle labelStyle = new Label.LabelStyle();
-        labelStyle.font = new BitmapFont(); // Font per defecte
-        Label label = new Label("Hola, això és un TextView", labelStyle);
-        label.setPosition(100, 400); // Posició del Label
-
-        // Crear un Button
-        Button button = new TextButton("Clica'm!", skin);
-        button.setPosition(100, 300); // Posició del Button
-        button.setSize(200, 50); // Mida del Button
-
-        // Afegir els actors al Stage
-        stage.addActor(label);
-        stage.addActor(button);
-
-        // Configurar l'Stage com a gestor d'entrada
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
         Gdx.input.setInputProcessor(stage);
+
+        // Crear una tabla para alinear elementos
+        Table table = new Table();
+        table.setFillParent(true); // Ocupa toda la pantalla
+        table.center(); // Centra los elementos dentro de la tabla
+
+        // Crear el Label
+        Label.LabelStyle labelStyle = new Label.LabelStyle();
+        labelStyle.font = new BitmapFont();
+        Label label = new Label("Hola, això és un TextView", labelStyle);
+
+        // Crear el Botón
+        TextButton button = new TextButton("Clica'm!", skin);
+
+        // Añadir a la tabla
+        table.add(label).padBottom(20); // Añade el label con un margen inferior
+        table.row(); // Nueva fila
+        table.add(button).width(200).height(50); // Tamaño del botón
+
+        // Añadir la tabla al stage
+        stage.addActor(table);
     }
 
     @Override
     public void render() {
-        // Netejar la pantalla
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        // Actualitzar i dibuixar l'Stage
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
     }
 
     @Override
     public void dispose() {
-        // Alliberar recursos
         stage.dispose();
         skin.dispose();
     }
